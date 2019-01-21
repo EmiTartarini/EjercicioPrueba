a020008d6412f5c7b686e42c1269e9c8f0f9d1de
EmiTartarini <34698661+EmiTartarini@users.noreply.github.com>
2019-01-21 09:40:40 -0600

    Emiliano Tartarini, Grupo 02

---
+++ velocidad promedio final.py
@@ -0,0 +1,9 @@
+#autor: Emiliano Tartarini
+#comentario: velocidad promedio
+
+tiempo = int(input('teclea el tiempo de viaje'))
+distancia = int(input('teclea el distancia de viaje'))
+
+velocidad = distancia/tiempo
+
+print('la velocidad promedio es:', velocidad)
